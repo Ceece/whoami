29f40780bebe5b62037bba3f4bd5b89d3109e841
Teerapong Manakul <imumi@msn.com>
2016-02-16 16:32:18 +0700

    Blank out boilerplate

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,9 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
-
-module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+module.exports = function (app) {
 
    app.route('/')
       .get(function (req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
-
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
 };
