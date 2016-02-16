c03d4c7637b879499684656140f8511c6194120b
Teerapong Manakul <imumi@msn.com>
2016-02-16 10:28:34 +0000

    Collect ip address

--- app/controllers/Whoami.server.js
+++ app/controllers/Whoami.server.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res) {
 	res.json({
-		ip: req.ip,
+		ipaddress: req.headers['x-forwarded-for'],
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].match(/\((.*?)\)/)[1]
 	});
